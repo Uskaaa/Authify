0788ae24f210ef0f5a00f8c49f25a43dfa88f35a
Bene <benedikt.haller@muehlbauer.de>
2025-06-25 14:26:34 +0200

    Update ForogPassword, ResetPassword

--- Authify/Authify.Core/Models/ForgotPasswordRequest.cs
+++ Authify/Authify.Core/Models/ForgotPasswordRequest.cs
@@ -1,6 +1,10 @@
-﻿namespace Authify.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authify.Core.Models;
 
 public class ForgotPasswordRequest
 {
+    [Required(ErrorMessage = "Please enter your email address")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string Email { get; set; } = string.Empty;
 }
--- Authify/Authify.Core/Models/ResetPasswordRequest.cs
+++ Authify/Authify.Core/Models/ResetPasswordRequest.cs
@@ -1,8 +1,24 @@
-﻿namespace Authify.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class ResetPasswordRequest
+namespace Authify.Core.Models;
+
+public class ResetPasswordRequest : IValidatableObject
 {
     public string Email { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter your new password")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
     public string NewPassword { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please confirm your new password")]
+    public string ConfirmNewPassword { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (NewPassword != ConfirmNewPassword)
+        {
+            yield return new ValidationResult("Passwords do not match", new[] { nameof(ConfirmNewPassword) });
+        }
+    }
 }
