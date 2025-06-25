1809783327fcde28737e4b97368b2c255f797aaf
Bene <benedikt.haller@muehlbauer.de>
2025-06-25 16:39:08 +0200

    Implement TwofactorClaimService

---
+++ Authify/Authify.Core/Interfaces/ITwoFactorClaimService.cs
@@ -0,0 +1,11 @@
+﻿using Authify.Core.Common;
+using Authify.Core.Models;
+
+namespace Authify.Core.Interfaces;
+
+public interface ITwoFactorClaimService
+{
+    Task<OperationResult> AddClaimAsync(string userId, string claimType, string claimValue);
+    Task<OperationResult> CheckClaimsAsync(string userId);
+    Task<OperationResult> RemoveClaimAsync(string userId, string claimType, string claimValue);
+}
---
+++ Authify/Authify.Core/Models/TwoFactorClaimUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Authify.Core.Models;
+
+public class TwoFactorClaimUser
+{
+    public string UserId { get; set; } = string.Empty;
+}
