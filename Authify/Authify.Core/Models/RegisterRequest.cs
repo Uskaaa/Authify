51927e3d9011ce6f113613a317b547c80bcd5509
Bene <benedikt.haller@muehlbauer.de>
2025-06-25 14:10:35 +0200

    Update Register.razor, Otp.razor and RegisterRequest

--- Authify/Authify.Core/Models/RegisterRequest.cs
+++ Authify/Authify.Core/Models/RegisterRequest.cs
@@ -1,8 +1,32 @@
-﻿namespace Authify.Core.Server.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class RegisterRequest
+namespace Authify.Core.Server.Models;
+
+public class RegisterRequest : IValidatableObject
 {
+    [Required(ErrorMessage = "Please enter your full name")]
+    [MinLength(2, ErrorMessage = "Please enter your full name")]
     public string Username { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter your email address")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string Email { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter your password")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
     public string Password { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please confirm your password")]
+    public string ConfirmPassword { get; set; }
+    
+    [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms and conditions")]
+    public bool TermsAccepted { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Password != ConfirmPassword)
+        {
+            yield return new ValidationResult("Passwords do not match", [nameof(ConfirmPassword)]);
+        }
+    }
 }
