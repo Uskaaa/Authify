263c5a1720173de57a124279d36e9583927307ff
Bene <benedikt.haller@muehlbauer.de>
2025-06-25 14:10:11 +0200

    Update Login.razor and LoginRequest

--- Authify/Authify.Core/Models/LoginRequest.cs
+++ Authify/Authify.Core/Models/LoginRequest.cs
@@ -1,8 +1,15 @@
-﻿namespace Authify.Core.Server.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authify.Core.Server.Models;
 
 public class LoginRequest
 {
+    [Required(ErrorMessage = "Please enter your email address")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string UsernameOrEmail { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter your password")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
     public string Password { get; set; } = string.Empty;
     public bool RememberMe { get; set; } = false;
 }
