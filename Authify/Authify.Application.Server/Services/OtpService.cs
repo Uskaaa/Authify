4ed796c5dac44218b8b76811f6f1a744a33d31b0
Uska <benedikthaller0@gmail.com>
2025-08-28 19:43:27 +0200

    Update OtpService
    Send Otp to chosen TwoFactorMethod

--- Authify/Authify.Application.Server/Services/OtpService.cs
+++ Authify/Authify.Application.Server/Services/OtpService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using Authify.Core.Interfaces;
 using Authify.Core.Models;
+using Authify.Core.Models.Enums;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -9,33 +10,45 @@ namespace Authify.Application.Services;
 public class OtpService : IOtpService
 {
     private readonly IMemoryCache _memoryCache;
-    private readonly IEmailSender _emailSender;
+    private readonly IDictionary<TwoFactorMethod, ITwoFactorHandler> _handlers;
     private readonly TimeSpan _otpExpiration = TimeSpan.FromMinutes(5);
     private readonly IDataProtectionProvider _dataProtectionProvider;
 
-    public OtpService(IMemoryCache memoryCache, IEmailSender emailSender, IDataProtectionProvider dataProtectionProvider)
+    public OtpService(IMemoryCache memoryCache, IEmailSender emailSender, ISmsSender smsSender, IDataProtectionProvider dataProtectionProvider)
     {
         _memoryCache = memoryCache;
-        _emailSender = emailSender;
         _dataProtectionProvider = dataProtectionProvider;
+        
+        // Handler Dictionary initialisieren
+        _handlers = new Dictionary<TwoFactorMethod, ITwoFactorHandler>
+        {
+            { TwoFactorMethod.Email, new EmailTwoFactorHandler(emailSender) },
+            { TwoFactorMethod.Sms, new SmsTwoFactorHandler(smsSender) }
+        };
     }
 
-    public async Task GenerateAndSendOtpAsync(string email)
+    public async Task GenerateAndSendOtpAsync(string usernameOrDestination, TwoFactorMethod method)
     {
         var otp = new Random().Next(100000, 999999).ToString();
+        _memoryCache.Set($"otp_{usernameOrDestination}", otp, _otpExpiration);
 
-        _memoryCache.Set($"otp_{email}", otp, _otpExpiration);
-
-        await _emailSender.SendEmailAsync(email, "Your OTP Code", $"Your OTP code is: {otp}");
+        if (_handlers.TryGetValue(method, out var handler))
+        {
+            await handler.SendOtpAsync(usernameOrDestination, otp);
+        }
+        else
+        {
+            throw new NotSupportedException($"TwoFactor method {method} is not supported.");
+        }
     }
 
-    public async Task<bool> ValidateOtpAsync(string email, string otpCode)
+    public async Task<bool> ValidateOtpAsync(string usernameOrDestination, string otpCode)
     {
-        if (_memoryCache.TryGetValue($"otp_{email}", out string? cachedOtp))
+        if (_memoryCache.TryGetValue($"otp_{usernameOrDestination}", out string? cachedOtp))
         {
             if (cachedOtp == otpCode)
             {
-                _memoryCache.Remove($"otp_{email}");
+                _memoryCache.Remove($"otp_{usernameOrDestination}");
                 return true;
             }
         }
--- Authify/Authify.Core/Interfaces/IOtpService.cs
+++ Authify/Authify.Core/Interfaces/IOtpService.cs
@@ -1,9 +1,11 @@
-﻿namespace Authify.Core.Interfaces;
+﻿using Authify.Core.Models.Enums;
+
+namespace Authify.Core.Interfaces;
 
 public interface IOtpService
 {
-    Task GenerateAndSendOtpAsync(string email);
-    Task<bool> ValidateOtpAsync(string email, string otpCode);
+    Task GenerateAndSendOtpAsync(string usernameOrDestination, TwoFactorMethod method);
+    Task<bool> ValidateOtpAsync(string usernameOrDestination, string otpCode);
     string GenerateToken(string email, bool rememberMe);
     (string email, bool rememberMe) ValidateToken(string token);
 }
