842ca71d456d7950e270c6ba79feb59ef2eb7f16
Uskaaa <127686495+Uskaaa@users.noreply.github.com>
2025-08-29 13:33:24 +0200

    Update AuthifyDbContext

--- Authify/Authify.Application.Server/Data/IAuthifyDbContext.cs
+++ Authify/Authify.Application.Server/Data/IAuthifyDbContext.cs
@@ -1,6 +1,11 @@
+using Authify.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Authify.Application.Data;
 
-public class IAuthifyDbContext
+public interface IAuthifyDbContext
 {
-    
+    DbSet<UserTwoFactor> UserSubscriptions { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
